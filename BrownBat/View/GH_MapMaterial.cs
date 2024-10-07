8237946790da239db6960a7ef78b2b021422be77
yyen <yyen@kglakademi.dk>
2024-10-07 16:20:42 +0200

    add icon to component

--- BrownBat/View/GH_MapMaterial.cs
+++ BrownBat/View/GH_MapMaterial.cs
@@ -110,9 +110,7 @@ namespace BrownBat.View
         {
             get
             {
-                //You can add image files to your project resources and access them like this:
-                // return Resources.IconForThisComponent;
-                return null;
+                return Properties.Resources.baticon;
             }
         }
 

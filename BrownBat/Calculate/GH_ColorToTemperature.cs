25df7c7bae21bc0ca2deda1cd5e675ab8007714a
yyen <yyen@kglakademi.dk>
2024-11-01 16:15:26 +0100

    add todo for adding structure temperature, and color to temperature

---
+++ BrownBat/Calculate/GH_ColorToTemperature.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+
+namespace BrownBat.Calculate
+{
+    public class GH_ColorToTemperature : GH_Component
+    {
+        /// <summary>
+        /// Initializes a new instance of the GH_WallColorToTemperature class.
+        /// </summary>
+        public GH_ColorToTemperature()
+          : base("GH_ColorToTemperature", "Nickname",
+              "Description",
+              "BrownBat", "Calculate")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+        }
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                //You can add image files to your project resources and access them like this:
+                // return Resources.IconForThisComponent;
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("1BEED20D-BEDF-4A21-9A1A-8AC4D98E909A"); }
+        }
+    }
+}
---
+++ BrownBat/Construct/GH_StructureTemperature.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+
+namespace BrownBat.Construct
+{
+    public class GH_StructureTemperature : GH_Component
+    {
+        /// <summary>
+        /// Initializes a new instance of the GH_StructureTemperature class.
+        /// </summary>
+        public GH_StructureTemperature()
+          : base("GH_StructureTemperature", "Nickname",
+              "Description",
+              "BrownBat", "Construct")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddGenericParameter("Structure", "S", "Constructed Bat Structure", GH_ParamAccess.item);
+            pManager.AddNumberParameter("Temperature", "T", "Tempertature for each Structure Pixel", GH_ParamAccess.tree);
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddGenericParameter("Structure", "S", "Bat Structure with Temperature", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+        }
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                //You can add image files to your project resources and access them like this:
+                // return Resources.IconForThisComponent;
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("4C75D047-3381-4C0B-AAF1-03A9A173708C"); }
+        }
+    }
+}

ff2572d99b3b48d4253297ccf5e3dca4d1dc8cc4
yyen <yyen@kglakademi.dk>
2025-06-16 14:09:22 +0200

    make 2 step join shatters to form all possibilites of joining. First join single shatters, then join group shatters.

--- BrownBat/Arrange/GH_MillBounds.cs
+++ BrownBat/Arrange/GH_MillBounds.cs
@@ -14,6 +14,7 @@ using NumSharp.Utilities;
 using Rhino;
 using Rhino.Geometry;
 using BrownBat.CalculateHelper;
+using Grasshopper;
 
 namespace BrownBat.Arrange
 {
@@ -37,8 +38,10 @@ namespace BrownBat.Arrange
             pManager.AddGenericParameter("ShatterGroups", "G", "Shatter Objects with group data", GH_ParamAccess.list);
             pManager.AddCurveParameter("Stock", "S", "Stock Boundary Curve", GH_ParamAccess.item);
             pManager.AddNumberParameter("Conductivity", "C", "Conductivity in pixel array position", GH_ParamAccess.tree);
-            pManager.AddIntegerParameter("JoinStart", "JO", "Start of Shatter Group. 2 parameters, true false for starting position.", GH_ParamAccess.list);
-            pManager.AddIntegerParameter("JoinDirection", "JD", "UV Direction. 4 Parameters, left = 0, right = 1, top = 2, bottom = 3.", GH_ParamAccess.list);
+            pManager.AddIntegerParameter("FirstStart", "FS", "Start of First Shatter Grouping. 2 parameters, true false for starting position.", GH_ParamAccess.list);
+            pManager.AddIntegerParameter("FirstDirection", "FD", "First UV Direction. 4 Parameters, left = 0, right = 1, top = 2, bottom = 3.", GH_ParamAccess.list);
+            pManager.AddIntegerParameter("SecondStart", "SS", "Start of Second Shatter Group. 2 parameters, true false for starting position.", GH_ParamAccess.list);
+            pManager.AddIntegerParameter("SecondDirection", "SD", "Second UV Direction. 4 Parameters, left = 0, right = 1, top = 2, bottom = 3.", GH_ParamAccess.list);
         }
 
         /// <summary>
@@ -61,14 +64,18 @@ namespace BrownBat.Arrange
             List<ShatterGroup> inBound = new List<ShatterGroup>();
             Curve inStock = default; 
             GH_Structure<GH_Number> inConductivity = new GH_Structure<GH_Number>();
-            List<int> inStart = new List<int>();
-            List<int> inDirection = new List<int>();
+            List<int> inFirstStart = new List<int>();
+            List<int> inFirstDirection = new List<int>();
+            List<int> inSecondStart = new List<int>();
+            List<int> inSecondDirection = new List<int>();
 
             DA.GetDataList(0, inBound);
             DA.GetData(1, ref inStock);
             DA.GetDataTree(2, out inConductivity);
-            DA.GetDataList(3, inStart);
-            DA.GetDataList(4, inDirection);
+            DA.GetDataList(3, inFirstStart);
+            DA.GetDataList(4, inFirstDirection);
+            DA.GetDataList(5, inSecondStart);
+            DA.GetDataList(6, inSecondDirection);
 
             Transform moveToWorld = Transform.PlaneToPlane(ShatterGroup.CutPlane, Plane.WorldXY);
             try { moveToWorld.TryGetInverse(out Transform moveBack); }
@@ -93,129 +100,11 @@ namespace BrownBat.Arrange
             int stockXCount = stockConductivity[0].Count;
 
             //join shatter
-            List<CuttingBound> cuttingBounds = new List<CuttingBound>();
-            //method
-
-            //var grouped = new ConcurrentBag<List<Brep>>();
-            //var visited = new ConcurrentDictionary<Brep, bool>();
-            var visitedBrep = new List<Brep>();
-            var groupedBrep = new List<List<Brep>>();
-
-            var allBounds = inBound.Select(bound => bound.Bounds).SelectMany(br => br).Distinct(new BrepComparer()).ToList();
-            var orderBounds = allBounds.OrderBy(b => AreaMassProperties.Compute(b).Area).ToList();
-
-            if (allBounds.Count() != inStart.Count)
-            { throw new Exception("Start parameter does not match object count!"); }
-
-            var startBounds = orderBounds.Zip(inStart, (br, st) => (br, st)).Where(grp => grp.st == 1).Select(grp => grp.br);
-            var startDirection = inDirection.Zip(inStart, (dir, st) => (dir, st)).Where(grp => grp.st == 1).Select(grp => grp.dir);
-
-            var boundWithDirection = startBounds.Zip(startDirection, (brep, dir) => (brep, dir)).ToList();
-
-            foreach(var rect in boundWithDirection)
-            {
-                //if (visited.ContainsKey(rect.brep)) return;
-                if(visitedBrep.Contains(rect.brep)) continue;
-
-                var neighbors = new List<Brep>();
-                var allNeighbor = new List<Brep>();
-                int rectIndex = 0;
-                switch (rect.dir)
-                {
-                    case (int)JoinDirection.Left:
-
-                        allNeighbor = inBound.Where(grp => CustomExtensions.Contains(grp.Bounds, rect.brep))
-                                           .Where(nbr => nbr.UGroupId != null)
-                                           .First()
-                                           .Bounds;
-
-                        rectIndex = allNeighbor.IndexOf(rect.brep);
-                        if (rectIndex == allNeighbor.Count - 1)
-                        {
-                            neighbors = allNeighbor;
-                        }
-                        else
-                        { 
-                            neighbors = allNeighbor.GetRange(0, rectIndex);
-                        }
-                        break;
-                    case (int)JoinDirection.Right:
-
-                        allNeighbor = inBound.Where(grp => CustomExtensions.Contains(grp.Bounds, rect.brep))
-                                           .Where(nbr => nbr.UGroupId != null)
-                                           .First()
-                                           .Bounds;
-                        rectIndex = allNeighbor.IndexOf(rect.brep);
-                        if (rectIndex == allNeighbor.Count - 1)
-                        {
-                            neighbors = allNeighbor;
-                        }
-                        else
-                        {
-                            neighbors = allNeighbor.GetRange(rectIndex + 1, allNeighbor.Count - rectIndex - 1);
-                        }
-                        break;
-                    case (int)JoinDirection.Top:
-                        allNeighbor = inBound.Where(grp => CustomExtensions.Contains(grp.Bounds, rect.brep))
-                                             .Where(nbr => nbr.VGroupId != null)
-                                             .First()
-                                             .Bounds;
-                        rectIndex = allNeighbor.IndexOf(rect.brep);
-                        if (rectIndex == allNeighbor.Count - 1)
-                        {
-                            neighbors = allNeighbor;
-                        }
-                        else
-                        {
-                            neighbors = allNeighbor.GetRange(0, rectIndex);
-                        }
-                        break;
-                    case (int)JoinDirection.Bottom:
-                        allNeighbor = inBound.Where(grp => CustomExtensions.Contains(grp.Bounds, rect.brep))
-                                           .Where(nbr => nbr.VGroupId != null)
-                                           .First()
-                                           .Bounds;
-                        rectIndex = allNeighbor.IndexOf(rect.brep);
-                        if (rectIndex == allNeighbor.Count - 1)
-                        {
-                            neighbors = allNeighbor;
-                        }
-                        else
-                        {
-                            neighbors = allNeighbor.GetRange(rectIndex + 1, allNeighbor.Count - rectIndex - 1);
-                        }
-                        break;
-                }
-
-               var group = FloodFill(rect.brep, neighbors, visitedBrep);
-               groupedBrep.Add(group);
-            }
-            var nonVisited = new List<Brep>();
-            foreach (var bound in orderBounds)
-            {
-                if (!visitedBrep.Contains(bound))
-                {
-                    nonVisited.Add(bound);
-                }
-            }
-
-            var shatter = groupedBrep.Select(grp => Brep.JoinBreps(grp, tolerance)).ToList();
-            List<Brep> pieces = new List<Brep>();
-            foreach (var join in shatter)
-            {
-                foreach(var j in join)
-                {
-                    j.MergeCoplanarFaces(tolerance, angleTolerance);
-                    pieces.Add(j);
-                }
-            }
-            pieces.AddRange(nonVisited);
-
-            foreach (var piece in pieces)
-            {
-                CuttingBound shatterToCut = new CuttingBound(piece);
-                cuttingBounds.Add(shatterToCut);
-            }
+            List<CuttingBound> firstGroup = JoinShatter(inBound, inFirstStart, inFirstDirection, tolerance, angleTolerance);
+            List<Brep> firstBrep = firstGroup.Select(s => s.Bound).ToList();
+            
+            List<ShatterGroup> firstShatter = uvGrouping(firstBrep);
+            List<CuttingBound> secondGroup = JoinShatter(firstShatter, inSecondStart, inSecondDirection, tolerance, angleTolerance);
 
             //calculate homogenity
             //get topleft bottom right startend domain
@@ -237,7 +126,8 @@ namespace BrownBat.Arrange
             var pieceBottomFifth = new List<double>();
             var pieceMean = new List<double>();
 
-            foreach (var bound in cuttingBounds)
+            List<Brep> resultBrep = new List<Brep>();
+            foreach (var bound in secondGroup)
             {
                 //get bound domian in start end
                 Brep moveBrep = bound.Bound.DuplicateBrep();
@@ -285,9 +175,11 @@ namespace BrownBat.Arrange
                 //CuttingBound.SetMean(bound, mean);
                 //CuttingBound.SetTopFifth(bound, topFifth);
                 //CuttingBound.SetLowFifth(bound, lowFifth);
+                resultBrep.Add(bound.Bound);
             }
 
-            DA.SetDataList(0, pieces);
+
+            DA.SetDataList(0, resultBrep);
             DA.SetDataList(1, pieceMean);
             DA.SetDataList(2, pieceTopFifth);
             DA.SetDataList(3, pieceBottomFifth);
@@ -372,6 +264,172 @@ namespace BrownBat.Arrange
         {
             NotStart = 0, Start = 1
         }
+        public List<CuttingBound> JoinShatter(List<ShatterGroup> shatters, List<int> start, List<int> direction, double tolerance, double angleTolerance)
+        {
+            //join shatter
+            List<CuttingBound> cuttingBounds = new List<CuttingBound>();
+            //method
+
+            //var grouped = new ConcurrentBag<List<Brep>>();
+            //var visited = new ConcurrentDictionary<Brep, bool>();
+            var visitedBrep = new List<Brep>();
+            var groupedBrep = new List<List<Brep>>();
+
+            var allBounds = shatters.Select(bound => bound.Bounds).SelectMany(br => br).Distinct(new BrepComparer()).ToList();
+            var orderBounds = allBounds.OrderBy(b => AreaMassProperties.Compute(b).Area).ToList();
+
+            List<int> trimStart = new List<int>();
+            List<int> trimDirection = new List<int>();
+
+            if (allBounds.Count != start.Count)
+            {
+                trimStart = start.GetRange(0, allBounds.Count);
+                trimDirection = direction.GetRange(0, allBounds.Count);
+            }
+            else
+            {
+                trimStart = start;
+                trimDirection = direction;
+            }
+
+            var startBounds = orderBounds.Zip(trimStart, (br, st) => (br, st)).Where(grp => grp.st == 1).Select(grp => grp.br);
+            var startDirection = trimDirection.Zip(trimStart, (dir, st) => (dir, st)).Where(grp => grp.st == 1).Select(grp => grp.dir);
+
+            var boundWithDirection = startBounds.Zip(startDirection, (brep, dir) => (brep, dir)).ToList();
+
+            foreach (var rect in boundWithDirection)
+            {
+                //if (visited.ContainsKey(rect.brep)) return;
+                if (visitedBrep.Contains(rect.brep)) continue;
+
+                var neighbors = new List<Brep>();
+                var allNeighbor = new List<Brep>();
+                int rectIndex = 0;
+                switch (rect.dir)
+                {
+                    case (int)JoinDirection.Left:
+
+                        allNeighbor = shatters.Where(grp => CustomExtensions.Contains(grp.Bounds, rect.brep))
+                                           .Where(nbr => nbr.UGroupId != null)
+                                           .First()
+                                           .Bounds;
+
+                        rectIndex = allNeighbor.IndexOf(rect.brep);
+                        if (rectIndex == allNeighbor.Count - 1)
+                        {
+                            neighbors = allNeighbor;
+                        }
+                        else
+                        {
+                            neighbors = allNeighbor.GetRange(0, rectIndex);
+                        }
+                        break;
+                    case (int)JoinDirection.Right:
+
+                        allNeighbor = shatters.Where(grp => CustomExtensions.Contains(grp.Bounds, rect.brep))
+                                           .Where(nbr => nbr.UGroupId != null)
+                                           .First()
+                                           .Bounds;
+                        rectIndex = allNeighbor.IndexOf(rect.brep);
+                        if (rectIndex == allNeighbor.Count - 1)
+                        {
+                            neighbors = allNeighbor;
+                        }
+                        else
+                        {
+                            neighbors = allNeighbor.GetRange(rectIndex + 1, allNeighbor.Count - rectIndex - 1);
+                        }
+                        break;
+                    case (int)JoinDirection.Top:
+                        allNeighbor = shatters.Where(grp => CustomExtensions.Contains(grp.Bounds, rect.brep))
+                                             .Where(nbr => nbr.VGroupId != null)
+                                             .First()
+                                             .Bounds;
+                        rectIndex = allNeighbor.IndexOf(rect.brep);
+                        if (rectIndex == allNeighbor.Count - 1)
+                        {
+                            neighbors = allNeighbor;
+                        }
+                        else
+                        {
+                            neighbors = allNeighbor.GetRange(0, rectIndex);
+                        }
+                        break;
+                    case (int)JoinDirection.Bottom:
+                        allNeighbor = shatters.Where(grp => CustomExtensions.Contains(grp.Bounds, rect.brep))
+                                           .Where(nbr => nbr.VGroupId != null)
+                                           .First()
+                                           .Bounds;
+                        rectIndex = allNeighbor.IndexOf(rect.brep);
+                        if (rectIndex == allNeighbor.Count - 1)
+                        {
+                            neighbors = allNeighbor;
+                        }
+                        else
+                        {
+                            neighbors = allNeighbor.GetRange(rectIndex + 1, allNeighbor.Count - rectIndex - 1);
+                        }
+                        break;
+                }
+
+                var group = FloodFill(rect.brep, neighbors, visitedBrep);
+                groupedBrep.Add(group);
+            }
+            var nonVisited = new List<Brep>();
+            foreach (var bound in orderBounds)
+            {
+                if (!visitedBrep.Contains(bound))
+                {
+                    nonVisited.Add(bound);
+                }
+            }
+
+            var shatter = groupedBrep.Select(grp => Brep.JoinBreps(grp, tolerance)).ToList();
+            List<Brep> pieces = new List<Brep>();
+            foreach (var join in shatter)
+            {
+                foreach (var j in join)
+                {
+                    j.MergeCoplanarFaces(tolerance, angleTolerance);
+                    pieces.Add(j);
+                }
+            }
+            pieces.AddRange(nonVisited);
+
+            foreach (var piece in pieces)
+            {
+                CuttingBound shatterToCut = new CuttingBound(piece);
+                cuttingBounds.Add(shatterToCut);
+            }
+            return cuttingBounds;
+        }
+
+        public List<ShatterGroup> uvGrouping(List<Brep> pieces)
+        {
+            var uBounds = pieces.GroupBy(p => Math.Round(AreaMassProperties.Compute(p).Centroid.Y, 3)).Select(grp => grp.ToList()).ToList();
+            var vBounds = pieces.GroupBy(p => Math.Round(AreaMassProperties.Compute(p).Centroid.X, 3)).Select(grp => grp.ToList()).ToList();
+
+            uBounds.ForEach(grp => grp.OrderBy(b => Math.Round(AreaMassProperties.Compute(b).Centroid.X, 3)));
+            vBounds.ForEach(grp => grp.OrderByDescending(b => Math.Round(AreaMassProperties.Compute(b).Centroid.Y, 3)));
+
+            //Convert to Bat Object
+            int groupId = 0;
+            List<ShatterGroup> shatterBounds = new List<ShatterGroup>();
+
+            foreach (var group in uBounds)
+            {
+                ShatterGroup uGroupObject = new ShatterGroup(group, groupId, null);
+                shatterBounds.Add(uGroupObject);
+                groupId++;
+            }
+            foreach (var group in vBounds)
+            {
+                ShatterGroup vGroupObject = new ShatterGroup(group, null, groupId);
+                shatterBounds.Add(vGroupObject);
+                groupId++;
+            }
+            return shatterBounds;
+        }
 
         class BrepComparer : IEqualityComparer<Brep>
         {
@@ -409,6 +467,8 @@ namespace BrownBat.Arrange
             }
 
         }
+
+
 
     }
 }
--- BrownBat/Components/ShatterGroup.cs
+++ BrownBat/Components/ShatterGroup.cs
@@ -22,26 +22,5 @@ namespace BrownBat.Components
             VGroupId = vGroupId;
         }
 
-        //public bool IsAdjacent(ShatterBound other)
-        //{
-        //    // Check for vertical/horizontal touching
-        //    bool vertical = X1 == other.X1 && X2 == other.X2 &&
-        //                    (Y2 == other.Y1 || other.Y2 == Y1);
-        //    bool horizontal = Y1 == other.Y1 && Y2 == other.Y2 &&
-        //                      (X2 == other.X1 || other.X2 == X1);
-        //    return vertical || horizontal;
-        //}
-        //public static void GetShatterEdges(ShatterBound shatterBound, Transform calculationPlane)
-        //{
-        //    Brep dupBrep = shatterBound.Bound.DuplicateBrep();
-        //    dupBrep.Transform(calculationPlane);
-        //    var shatterVertices = dupBrep.DuplicateVertices();
-
-        //    var shatterCorners = shatterVertices.OrderBy(pts => pts.X).ThenBy(pts => pts.Y).ToList();
-        //    shatterBound.X1 = shatterCorners[0].X;
-        //    shatterBound.Y1 = shatterCorners[0].Y;
-        //    shatterBound.X2 = shatterCorners[3].X;
-        //    shatterBound.Y2 = shatterCorners[3].Y;
-        //}
     }
 }
